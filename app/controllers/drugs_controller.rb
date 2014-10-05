4d3dc4dc165ab934bbe84a526beb445d363b9a05
David <danich1@umbc.edu>
2014-10-04 22:58:20 -0400

    FIXED THE ISSUE OMG!!!!!

--- app/controllers/drugs_controller.rb
+++ app/controllers/drugs_controller.rb
@@ -7,9 +7,10 @@ class DrugsController < ApplicationController
     @drugs = Drug.all
   end
 
-  # GET /drugs/1
-  # GET /drugs/1.json
+  # GET /drugs/:name
+  # GET /drugs/:name.json
   def show
+    @response = Drug.main(params[:name], params[:count])
   end
 
   # GET /drugs/new
@@ -53,8 +54,8 @@ class DrugsController < ApplicationController
 
   # POST /drugs/search
   def search
-    @response = Drug.main(params[:name], params[:quantity])
-    render json: @response
+    puts "#{params}"
+    redirect_to drug_url(:count => 20,:id=>params[:name])
   end
 
   # DELETE /drugs/1
@@ -70,7 +71,7 @@ class DrugsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_drug
-      @drug = Drug.find(params[:id])
+      #@drug = Drug.main(params[:name], 4)
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- app/models/drug.rb
+++ app/models/drug.rb
@@ -10,7 +10,7 @@ class Drug < ActiveRecord::Base
 			puts 'no match'
 		end
 		# print the results
-		return results
+		return count, results
 	end
 
 	# query the Gene Variants API
@@ -18,10 +18,7 @@ class Drug < ActiveRecord::Base
 		auth = {:username => "Hackathon_07", :password => "MT79CL3S54RXZH2B"}
 		end_point = "https://lsapi-demo.thomson-pharma.com/ls-api-ws/ws/rs/drugs-v1/drug"
 		puts "#{query} is my query"
-		response = HTTParty.get("#{end_point}/search?query=phaseHighest:#{query}&offset=#{offset}&hits=#{hits}", :digest_auth => auth)
-		if response.nil?
-			puts "sad face"
-		end
+		response = HTTParty.get("#{end_point}/search?query=#{query}&offset=#{offset}&hits=#{hits}", :digest_auth => auth)
 		puts "#{response} is my repsonse from the query"
 		count = response['drugResultsOutput']['totalResults'].to_i
 		return count, response
