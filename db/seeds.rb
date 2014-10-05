87784d607bb5452b96b86e955d277cd035bc7edb
David <danich1@umbc.edu>
2014-10-05 03:15:20 -0400

    CAN ADD DATA NOW

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+patients_list = [
+	["Matt", "Doomstone", "1000 Hiltop Circle", "Catonsvile", "MD", "21258", Time.now],
+	["Joe", "Skyes", "1000 Park Street", "Jacksonville", "FL", "32099", Time.now],
+	["Larry", "Williams", "100 Main Street", "Boston", "MA", "02201", Time.now],
+	["John", "Dingleberry", "20 Massachusets Street", "Lawrence", "KS", "66044", Time.now]
+	]
+
+patients_list.each do |patient|
+	Patient.create(:first_name => patient[0], :last_name => patient[1], :address => patient[2], :city => patient[3], :state => patient[4], :zip => patient[5], :created_at => patient[6], :updated_at => patient[6])
+end
+
+symptom_list = [
+	["Diabetes mellitus", Time.now, 1],
+	["Obesity", Time.now, 1],
+	["Acute lung injury", Time.now, 2],
+	["Neonatal respiratory distress syndrome", Time.now, 2],
+	["Hepatitis C virus infection", Time.now, 3],
+	["Cancer", Time.now, 4],
+	["Cystic fibrosis", Time.now, 4],
+]
+
+symptom_list.each do |symptoms|
+	Symptom.create(:name => symptoms[0], :created_at => symptoms[1], :updated_at => symptoms[1], :patient_id => symptoms[2])
+end	
